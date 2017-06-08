bfb1a03dd79a2cdde9d2df73bfcde16582bebdc0
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-06-08 10:11:18 -0600

    Added 1 example of graph structure

--- pathnet_unittests.py
+++ pathnet_unittests.py
@@ -1,2 +1,10 @@
 
-### All of the unit tests  
+### All of the unit tests for pathnet
+
+ graph_structure = [ [ ((2,2)) ],
+                    [ ((2,2), identity_module), ((2,2), identity_module), ((3,3), identity_module) ],
+                    [ ((2,2), identity_module), ((2,2), identity_module), ((3,3), identity_module) ],
+                    [ ((2,2), identity_module), ((3,3), identity_module), ((4,4), identity_module) ],
+                    [ ((2,2), identity_module), ((2,2), identity_module), ((3,3), identity_module) ],
+                    [ ((2,2), identity_module) ]
+                  ]
