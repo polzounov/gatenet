504c164f233f376f4123fa0406bb5d64bb594fa8
chrisnielsen <christopher.nielsen93@gmail.com>
2017-07-07 15:49:49 -0600

    Testing Linux System

--- preprocessing.py
+++ preprocessing.py
@@ -6,4 +6,4 @@ def preprocess_data(data):
 def add_random_noise(data):
     for i in range(len(data)):
         rand_num = np.random.rand(len(data[0]))
-        data[i, rand_num > 0.5] = np.minimum(data[i, rand_num > 0.5] + rand_num[rand_num > 0.5], 1.0);
+        data[i,  rand_num > 0.5] = np.minimum(data[i, rand_num > 0.5] + rand_num[rand_num > 0.5], 1.0);
