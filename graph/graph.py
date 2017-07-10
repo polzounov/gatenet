e005b2c846c01f8ec79422a39eaac3e0d321a59a
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-07-10 13:22:33 -0600

    Moved section of code to init

--- graph/graph.py
+++ graph/graph.py
@@ -7,7 +7,7 @@ from graph.layer import *
 from graph.sublayer import *
 from graph.module import *
 
-######################################################################
+##############################################################################
 ## Code for Gatenet Graph construction
 class Graph():
 
@@ -17,19 +17,18 @@ class Graph():
         self.output_layer = None
         self.sublayers = None
 
-        # Build new graph from param dict
-        self.build_graph(parameter_dict)
-
-    ## Build graph to test code
-    def build_graph(self, parameter_dict):
-
-        ## Define Hyperparameters #########################################################
+        ## Define Hyperparameters
         self.M = parameter_dict['M']
         self.L = parameter_dict['L']
         self.tensor_shape = parameter_dict['tensor_shape']
-        self.gamma = parameter_dict['gamma']        
+        self.gamma = parameter_dict['gamma']     
 
-        ## Define Layers #################################################################
+        # Build new graph from param dict
+        self.build_graph(parameter_dict)
+
+
+    def build_graph(self, parameter_dict):
+        ## Define Layers #####################################################
         prev_output_shape = 784 # Image size (first input)
         self.gated_layers = []
         for i in range(self.L):
@@ -49,25 +48,26 @@ class Graph():
                              'module_type': LinearModule,
                              'sublayer_type': AdditionSublayerModule}
         self.output_layer = GatedLayer(output_layer_defn, gamma=0)
-        ##################################################################################
+        ######################################################################
 
 
-    ## Build graph to test code
     def return_logits(self, input_images):
-        ## Construct graph ###############################
+        '''Return the output of graph based off of input_images'''
+        ## Construct graph ###################################################
         next_input = input_images
         for i in range(self.L):
             next_input = self.gated_layers[i].process_layer(next_input)
         logits = self.output_layer.process_layer(next_input)
-        ##################################################
+        ######################################################################
         return logits
 
 
     def determine_gates(self, image, x, sess):
+        '''Output the values of the gates for image'''
         gates = np.zeros((self.L, self.M))
         for i in range(len(self.gated_layers)):
             g = sess.run([self.gated_layers[i].gates],
                          feed_dict={x:image})
             gates[i] = np.array(g)
         return gates
-######################################################################
+##############################################################################
