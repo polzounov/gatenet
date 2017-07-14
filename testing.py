f35601856ae35d806c276c231511ed2c72e501e8
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-07-14 11:34:50 -0600

    Added imports to improve python 2/3 compatibility

--- testing.py
+++ testing.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import pickle
 # For module and submodule type
 from graph.sublayer import *
