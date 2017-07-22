0134e11aa205b8b7c7566c5e73b66e4f848dcb96
chrisnielsen <christopher.nielsen93@gmail.com>
2017-07-22 14:21:50 -0600

    changed structure of data manager and finished adding omniglot

---
+++ tiny_imagenet_loading/dManager.py
@@ -0,0 +1,140 @@
+import os
+import numpy as np
+import time
+from PIL import Image
+import matplotlib.pyplot as plt
+from tiny_imagenet_loading.dataset_parameters import *
+
+plt.interactive(False)
+
+#########################
+#########################
+#########################
+
+## Set path to tiny-imagenet folder
+path = "/home/chris/tiny-imagenet-200"
+
+#########################
+#########################
+
+
+class DataManager:
+
+    def __init__(self, path, dataset='tiny-imagenet', load_images_in_memory = False ):
+
+        # set path to tiny-imagenet folder
+        self.path = path
+        self.dataset = load_dataset(path, dataset, load_images_in_memory)
+        self.classes = self.dataset.classes
+        self.images_per_class = self.dataset.images_per_class
+        self.image_size = self.dataset.image_size
+
+
+
+    def build_dataset(self):
+        pass
+
+
+
+    def get_train_batch(self):
+        pass
+
+
+
+class MetaDataManager(DataManager):
+    def __init__(self,path, dataset='tiny-imagenet', load_images_in_memory = False):
+        super().__init__(path, dataset, load_images_in_memory)
+
+
+    def build_dataset(self, num_dataset_classes=5, k_shot=5, dataset_num_test=5 ):
+        self.k_shot = k_shot
+        self.num_dataset_classes = num_dataset_classes
+        self.dataset_num_test = dataset_num_test
+
+        # pick classes for dataset
+        self.dataset_classes = np.random.choice(self.classes, self.num_dataset_classes, replace=False)
+
+        # pick images for dataset
+        self.dataset_train_images = np.empty((self.num_dataset_classes, self.images_per_class - dataset_num_test),
+                                             dtype=np.uint8)
+        self.dataset_train_labels = np.empty((self.num_dataset_classes, self.images_per_class - dataset_num_test),
+                                             dtype=np.int32)
+
+        self.dataset_test_images = np.empty((self.num_dataset_classes, dataset_num_test), dtype=np.uint8)
+        self.dataset_test_labels = np.empty((self.num_dataset_classes, dataset_num_test), dtype=np.int32)
+
+        for i in range(self.num_dataset_classes):
+            arr = np.arange(self.images_per_class)
+            np.random.shuffle(arr)
+
+            self.dataset_train_images[i] = arr[:self.images_per_class - dataset_num_test]
+            self.dataset_train_labels[i] = np.repeat(self.dataset_classes[i], self.images_per_class - dataset_num_test)
+
+            self.dataset_test_images[i] = arr[self.images_per_class - dataset_num_test:]
+            self.dataset_test_labels[i] = np.repeat(self.dataset_classes[i], dataset_num_test)
+
+    def get_train_batch(self):
+        batch_size = self.k_shot * self.num_dataset_classes
+        batch_images = np.zeros((batch_size, self.image_size), dtype=np.uint8)
+        batch_labels = np.zeros((batch_size, 1), dtype=np.int32)
+
+        count = 0
+        for i in range(self.num_dataset_classes):
+            elems = np.random.choice(len(self.dataset_train_images[i]), self.k_shot, replace=False)
+            for j in range(self.k_shot):
+                batch_labels[count] = self.dataset_train_labels[i][elems[j]]
+                batch_images[count] = self.dataset.aquire_image(self.dataset_train_labels[i][elems[j]], elems[j])
+                count = count + 1
+
+        return batch_images, batch_labels
+
+
+k_fold = 5
+num_classes = 5
+
+tiny_imagenet_path = "/home/chris/tiny-imagenet-200"
+omniglot_path = "/home/chris/images_background"
+
+
+
+imagenet_metaDataManager = MetaDataManager(tiny_imagenet_path, dataset='tiny-imagenet')
+imagenet_metaDataManager.build_dataset(k_fold,num_classes,100)
+images, labels = imagenet_metaDataManager.get_train_batch()
+
+
+plt.figure(1)
+count = 0
+for i in range(num_classes):
+    for j in range(k_fold):
+        plt.subplot(num_classes,k_fold,count+1)
+        imgplot = plt.imshow(np.reshape(images[count], (64,64,3)))
+        count = count + 1
+#plt.show()
+
+
+
+
+
+
+omniglot_metaDataManager = MetaDataManager(omniglot_path,dataset='omniglot')
+omniglot_metaDataManager.build_dataset(k_fold,num_classes,5)
+images, labels = omniglot_metaDataManager.get_train_batch()
+
+
+plt.figure(2)
+count = 0
+for i in range(num_classes):
+    for j in range(k_fold):
+        plt.subplot(num_classes,k_fold,count+1)
+        imgplot = plt.imshow(np.reshape(images[count], (105,105)))
+        count = count + 1
+plt.show()
+
+
+gg = 1
+
+
+
+
+
+
--- tiny_imagenet_loading/dataset_parameters.py
+++ tiny_imagenet_loading/dataset_parameters.py
@@ -1,6 +1,190 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+plt.interactive(False)
+
+
+class DataSet:
+
+    def __init__(self, path, images_in_memory):
+        self.path = path
+        self.image_pointers = None
+        self.classes = None
+        self.images_per_class = None
+        self.image_size = None
+        self.images = None
+        self.images_in_memory = images_in_memory
+
+        self.get_image_file_pointers()
+
+
+    def get_image_file_pointers(self):
+        pass
+
+    def aquire_image(self, class_num, image_num):
+        pass
+
+    def load_images_in_memory(self):
+        # if load_images_in_memory is true, read all images into a giant matrix
+        # the size will be about 1.2 GB for tiny-imagenet
+        self.images_in_memory = True
+        self.images = np.zeros((self.classes, self.images_per_class, self.image_size), dtype=np.uint8)
+
+        print('loading images in memory')
+        for c in range(self.classes):
+            for i in range(self.images_per_class):
+                img = Image.open(self.image_pointers[c][i])
+                img.load()
+                data = np.asarray(img, dtype="uint8")
+
+                # imgplot = plt.imshow(data)
+                # plt.show()
+
+                data = np.reshape(data, (-1,))
+
+                # if image is grayscale then tile image to form equivalent RGB representation
+                if data.shape[0] == 64 * 64:
+                    data = np.tile(data, 3)
+
+                self.images[c][i] = data
+
+        print('images loaded')
 
 
 
-def get_dataset_parameters(dataset='tiny-imagenet'):
+class TinyImagenet(DataSet):
+    def __init__(self, path, images_in_memory):
+        super().__init__(path, images_in_memory)
+
+
+    def get_image_file_pointers(self):
+        # store file location to all images in image_pointers
+        self.image_pointers = []
+
+        class_folder = self.path + "/train"
+        class_contents = os.listdir(class_folder)
+
+
+        for i in range(len(class_contents)):
+            folder = self.path + "/train/" + class_contents[i].strip() + '/images'
+            contents = os.listdir(folder)
+            contents = [folder + '/' + s for s in contents]
+            self.image_pointers.append(contents)
+
+        # number of classes, 200 for tiny-imagenet
+        self.classes = len(self.image_pointers)
+
+        # number of images per classes, 500 for tiny-imagenet
+        self.images_per_class = len(self.image_pointers[0])
+
+        # size of image
+        self.image_size = 64 * 64 * 3
+
+        if self.images_in_memory == True:
+            self.load_images_in_memory()
+
+    def aquire_image(self, class_num, image_num):
+        if self.images_in_memory == True:
+            return self.images[class_num][image_num]
+
+        img = Image.open(self.image_pointers[class_num][image_num])
+        img.load()
+        data = np.asarray(img, dtype="uint8")
+        data = np.reshape(data, (-1,))
+
+        # if image is grayscale then tile image to form equivalent RGB representation
+        if data.shape[0] == 64 * 64:
+            data = np.tile(data, 3)
+
+        return data
+
+
+
+
+
+class Omniglot(DataSet):
+    def __init__(self, path, images_in_memory):
+        super().__init__(path, images_in_memory)
+
+
+    def get_image_file_pointers(self):
+        # store file location to all images in image_pointers
+        self.image_pointers = []
+
+        class_folder = self.path
+        class_contents = os.listdir(class_folder)
+
+
+        for i in range(len(class_contents)):
+
+            character_folder = self.path + '/' + class_contents[i].strip()
+            character_contents = os.listdir(character_folder)
+
+            for j in range(len(character_contents)):
+                folder = self.path + '/' + class_contents[i].strip() + '/' + character_contents[j].strip() + '/'
+                contents = os.listdir(folder)
+                contents = [folder + '/' + s for s in contents]
+                self.image_pointers.append(contents)
+
+        # number of classes, 200 for tiny-imagenet
+        self.classes = len(self.image_pointers)
+
+        # number of images per classes, 500 for tiny-imagenet
+        self.images_per_class = len(self.image_pointers[0])
+
+        # size of image
+        self.image_size = 105*105
+
+        if self.images_in_memory == True:
+            self.load_images_in_memory()
+
+    def aquire_image(self, class_num, image_num):
+        if self.images_in_memory == True:
+            return self.images[class_num][image_num]
+
+        img = Image.open(self.image_pointers[class_num][image_num])
+        img.load()
+        data = np.asarray(img, dtype="uint8")
+        data = np.reshape(data, (-1,))
+
+        # if image is grayscale then tile image to form equivalent RGB representation
+        #if data.shape[0] == 64 * 64:
+         #   data = np.tile(data, 3)
+
+        return data
+
+
+
+
+
+
+
+
+
+
+def load_dataset(path, dataset='tiny-imagenet', load_images_into_memory=False):
     if dataset == 'tiny-imagenet':
-        return {'training_images'}
+        return TinyImagenet(path, load_images_into_memory)
+    if dataset == 'omniglot':
+        return Omniglot(path, load_images_into_memory)
+
+
+
+
+
+
+'''
+
+## Set path to tiny-imagenet folder
+path = "/home/chris/images_background"
+tt = Omniglot(path, False)
+
+img = tt.aquire_image(10,10)
+
+
+plt.figure(1)
+imgplot = plt.imshow(np.reshape(img, (105,105)))
+plt.show()
+'''
