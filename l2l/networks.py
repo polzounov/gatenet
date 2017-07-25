766b13f0d17f57a9711c21b2c4bde7bc57628543
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-07-25 12:43:41 -0600

    Changed networks

--- l2l/networks.py
+++ l2l/networks.py
@@ -33,129 +33,7 @@ import six
 import sonnet as snt
 import tensorflow as tf
 
-import preprocess
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-################################################################################
-class DeepLSTM(snt.AbstractModule):
-    """docstring for LSTM"""
-    def __init__(self,
-                 output_size,
-                 batch_size=1,
-                 layers=(5, 5), # Each el is the hidden size of the respective RNN cell (for deep RNNs)
-                 scale=1.0,
-                 name='LSTM'):
-        super(DeepLSTM, self).__init__(name=name)
-        self._batch_size = batch_size
-        self._layers = layers
-        self._scale = scale
-
-        # Create cores (layers of a single RNN timestep)
-        self._cores = []
-        for i, size in enumerate(self._layers, start=1):
-            name = "lstm_{}".format(i)
-
-            ######################
-            ## Test Code
-            init = _get_layer_initializers(None, name,
-                                           ("w_gates", "b_gates"))
-            ######################
-
-            self._cores.append(snt.LSTM(size, name=name, initializers=init)) # TODO INIT
-
-        # Create the Deep RNN
-        self._rnn = snt.DeepRNN(self._cores, skip_connections=False, name="deep_rnn")
-
-        ######################
-        ## Test Code
-        init = _get_layer_initializers(None, "linear", ("w", "b"))
-        ######################
-
-        # Linear layer to get the output
-        self._linear = snt.Linear(output_size, name="linear", initializers=init) # TODO INIT
-
-        # Set the initial hidden state
-        self.prev_state = self._rnn.initial_state(self._batch_size, **kwargs)
-
-    def _build(self, inputs):
-        ##inputs = self._preprocess(tf.expand_dims(inputs, -1))
-        # Incorporates preprocessing into data dimension
-        inputs = tf.reshape(inputs, [inputs.get_shape().as_list()[0], -1])
-        output, next_state = self._rnn(inputs, self.prev_state)
-        self.prev_state = next_state
-        return self._linear(output) * self._scale
-
-
-
-class CoordinateWiseDeepLSTM(DeepLSTM):
-    """docstring for LSTM"""
-
-    def __init__(self,
-                 output_size,
-                 batch_size=1,
-                 layers=(5, 5),  # Each el is the hidden size of the respective RNN cell (for deep RNNs)
-                 scale=1.0,
-                 name='LSTM'):
-        super(CoordinateWiseDeepLSTM, self).__init__(output_size,batch_size, layers, scale,name)
-
-
-    def _build(self, inputs):
-        ##inputs = self._preprocess(tf.expand_dims(inputs, -1))
-        # Incorporates preprocessing into data dimension
-
-        outputs = inputs
-        for i in range(inputs.get_shape().as_list()[0]):
-            input = inputs[inputs.get_shape().as_list()[0]][i]
-
-            output, next_state = self._rnn(inputs, self.prev_state[i])
-            self.prev_state[i] = next_state[i]
-            outputs[i] = output
-        return self._linear(outputs) * self._scale
-
-################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import learning_to_learn.preprocess
 
 
 def factory(net, net_options=(), net_path=None):
@@ -278,6 +156,74 @@ def _get_layer_initializers(initializers, layer_name, fields):
   return _get_initializers(initializers, fields)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+class DeepLSTM(Network):
+  """docstring for LSTM"""
+
+  def __init__(self,
+               output_size,
+               batch_size=1,
+               layers=(5, 5),  # Each el is the hidden size of the respective RNN cell (for deep RNNs)
+               scale=1.0,
+               name='LSTM'):
+    super(LSTM, self).__init__(name=name)
+    self._batch_size = batch_size
+    self._layers = layers
+    self._scale = scale
+
+    with tf.variable_scope(self._template.variable_scope):
+      # Create cores (layers of a single RNN timestep)
+      self._cores = []
+      for i, size in enumerate(self._layers, start=1):
+        name = "lstm_{}".format(i)
+        init = _get_layer_initializers(initializer, name, ("w_gates", "b_gates"))
+        self._cores.append(snt.LSTM(size, name=name, initializers=init))
+
+      # Create the Deep RNN
+      self._rnn = snt.DeepRNN(self._cores, skip_connections=False, name="deep_rnn")
+
+      # Linear layer to get the output
+      init = _get_layer_initializers(initializer, "linear", ("w", "b"))
+      self._linear = snt.Linear(output_size, name="linear", initializers=init)
+
+    # Set the initial hidden state
+    self.prev_state = self._rnn.initial_state(self._batch_size, **kwargs)
+
+
+def _build(self, inputs):
+  ##inputs = self._preprocess(tf.expand_dims(inputs, -1))
+  # Incorporates preprocessing into data dimension
+  inputs = tf.reshape(inputs, [inputs.get_shape().as_list()[0], -1])
+  output, next_state = self._rnn(inputs, self.prev_state)
+  self.prev_state = next_state
+  return self._linear(output) * self._scale
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ################################################################################
 class StandardDeepLSTM(Network):
   """LSTM layers with a Linear layer on top."""
@@ -346,7 +292,6 @@ class StandardDeepLSTM(Network):
 
 
 ################################################################################
-'''
 class CoordinateWiseDeepLSTM(StandardDeepLSTM):
   """Coordinate-wise `DeepLSTM`."""
 
@@ -390,7 +335,7 @@ class CoordinateWiseDeepLSTM(StandardDeepLSTM):
     return super(CoordinateWiseDeepLSTM, self).initial_state_for_inputs(
         reshaped_inputs, **kwargs)
 
-'''
+
 ################################################################################
 class KernelDeepLSTM(StandardDeepLSTM):
   """`DeepLSTM` for convolutional filters.
