08d1e7016128ceb5f980151396bad84f5d37e30e
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-07-25 12:19:27 -0600

    Reordered networks file

--- l2l/networks.py
+++ l2l/networks.py
@@ -1,11 +1,54 @@
+# The following code is taken from the learning to learn library from deepmind
+# https://github.com/deepmind/learning-to-learn
+#==============================================================================
+
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Learning 2 Learn meta-optimizer networks."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-import sonnet as snt
-import numpy as np
+import abc
+import collections
+import sys
 
+import dill as pickle
+import numpy as np
+import six
+import sonnet as snt
+import tensorflow as tf
+
+import preprocess
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
 class DeepLSTM(snt.AbstractModule):
     """docstring for LSTM"""
     def __init__(self,
@@ -81,43 +124,38 @@ class CoordinateWiseDeepLSTM(DeepLSTM):
             outputs[i] = output
         return self._linear(outputs) * self._scale
 
-
-# The following code is taken from the learning to learn library from deepmind
-# https://github.com/deepmind/learning-to-learn
-#==============================================================================
+################################################################################
 
 
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Learning 2 Learn meta-optimizer networks."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import abc
-import collections
-import sys
 
-import dill as pickle
-import numpy as np
-import six
-import sonnet as snt
-import tensorflow as tf
 
-import preprocess
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def factory(net, net_options=(), net_path=None):
