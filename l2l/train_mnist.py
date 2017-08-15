336a60829f2494bcd1ee8d2e6a2cdd5b6d0fd764
chrisnielsen <christopher.nielsen93@gmail.com>
2017-08-15 13:45:09 -0600

    added gatenet for mnist

--- l2l/train_mnist.py
+++ l2l/train_mnist.py
@@ -14,90 +14,6 @@ from timing import *
 
 
 
-def selu(x):
-    alpha = 1.6732632423543772848170429916717
-    scale = 1.0507009873554804934193349852946
-    #return scale * tf.where(x >= 0.0, x, alpha * tf.exp(x) - alpha)
-    return scale * tf.contrib.keras.activations.elu(x, alpha)
-
-
-class MLP():
-    def __init__(self,
-                 x,
-                 y_,
-                 hidden_sizes=[10,10,10],
-                 scope = 'init_graph',
-                 activation=selu):
-
-        _, b = x.get_shape().as_list()
-        _, d = y_.get_shape().as_list()
-        #tf.contrib.keras.initializers.he_uniform()#
-        init_w = tf.contrib.layers.xavier_initializer(uniform=True, dtype=tf.float32)#
-        init_b = tf.constant_initializer(0)
-
-        self.scope = scope
-        self.hidden_sizes = hidden_sizes
-        self.input_size  = b
-        self.output_size = d
-        self.act = activation
-
-        with tf.variable_scope(self.scope):
-            ws = []
-            bs = []
-            prev_hidden = self.input_size
-            # Hidden layers
-            for i, hidden_size in enumerate(hidden_sizes):
-                with tf.variable_scope('layer'+str(i)):
-                    w = tf.get_variable('w'+str(i),[prev_hidden, hidden_size], initializer=init_w)
-                    b = tf.get_variable('b'+str(i), [hidden_size], initializer=init_b)
-                    ws.append(w)
-                    bs.append(b)
-                    prev_hidden = hidden_size
-
-            # Ouput layer
-            with tf.variable_scope('layer_out'):
-                w = tf.get_variable('w_out', [prev_hidden, self.output_size], initializer=init_w)
-                b = tf.get_variable('b_out', [self.output_size], initializer=init_b)
-                ws.append(w)
-                bs.append(b)
-    def run(self, x, custom_getter=None):
-        # Get the variables
-        with tf.variable_scope(self.scope, reuse=True):
-            ws = []
-            bs = []
-            # Hidden layers
-            for i, hidden_size in enumerate(self.hidden_sizes):
-                with tf.variable_scope('layer'+str(i)):
-                    w = tf.get_variable('w'+str(i), custom_getter=custom_getter)
-                    b = tf.get_variable('b'+str(i), custom_getter=custom_getter)
-                    ws.append(w)
-                    bs.append(b)
-            # Ouput layer
-            with tf.variable_scope('layer_out'):
-                w = tf.get_variable('w_out', custom_getter=custom_getter)
-                b = tf.get_variable('b_out', custom_getter=custom_getter)
-                ws.append(w)
-                bs.append(b)
-
-        # Save the summaries
-        with tf.name_scope('mlp_run_summaries'):
-            for i, (w, b) in enumerate(zip(ws, bs)):
-                variable_summaries(w, name='w_'+str(i))
-                variable_summaries(b, name='b_'+str(i))
-
-        # Run the graph with weights ws and biases bs
-        with tf.name_scope('run_graph'):
-            # Process the layers
-            prev_out = x
-            # Process hidden layers
-            for i, (w, b) in enumerate(zip(ws[:-1], bs[:-1])):
-                with tf.variable_scope('layer'+str(i)):
-                    prev_out = self.act(tf.matmul(prev_out, w) + b)
-            # Process output layer
-            with tf.variable_scope('layer_out'):
-                return self.act(tf.matmul(prev_out, ws[-1]) + bs[-1])
-
-
 ################################################################################
 ######                        MAIN PROGRAM                                ######
 ################################################################################
@@ -105,32 +21,38 @@ def train(parameter_dict):
     with tf.Session() as sess:
 
         k_shot = 4
-        num_classes = 5
+        num_classes = parameter_dict['C']
         num_test_images = 5
         mnist_path = '../datasets/mnist'
         metaDataManager = MetaDataManager(mnist_path, dataset='mnist', load_images_in_memory=False)
         metaDataManager.build_dataset(num_classes, k_shot, num_test_images)
 
         # Input placeholders
-        x = tf.placeholder(tf.float32, [None, metaDataManager.dataset.image_size_matrix[0]*
-                                        metaDataManager.dataset.image_size_matrix[1]*
+        x = tf.placeholder(tf.float32, [None, metaDataManager.dataset.image_size_matrix[0],
+                                        metaDataManager.dataset.image_size_matrix[1],
                                         metaDataManager.dataset.image_size_matrix[2]], name='x-input')
         y_ = tf.placeholder(tf.float32, [None, num_classes], name='y-input')
 
 
         ########## Define simple graph #########################################
-        graph = MLP(x, y_)
+        graph = Graph(parameter_dict)
 
         ########## Build the rest of the functions in the graph ################
-        def loss_func(x=x, y_=y_, fx=graph.run, custom_getter=None):
+        def loss_func(x=x, y_=y_, fx=graph.return_logits, mock_func=None, var_dict=None):
             def build():
-                y = fx(x, custom_getter=custom_getter)
+                y = fx(x)  # , custom_getter=custom_getter)
                 with tf.name_scope('loss'):
                     return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+            # Run through the mock func (w fake vars)
+            if (mock_func is not None) and (var_dict is not None):
+                return mock_func(build, var_dict)
+            # Return using real vars
             return build
 
         # Get the y, loss, and accuracy to use in printing out stuff later
-        y = graph.run(x)
+        y = graph.return_logits(x)
+
 
         with tf.name_scope('loss'):
             loss = loss_func()()
@@ -148,10 +70,10 @@ def train(parameter_dict):
         shared_scopes = ['init_graph']
 
         # Meta optimization
-        optimizer = MetaOptimizer(shared_scopes, name='MetaOptSimple')
-        train_step, train_step_meta = optimizer.minimize(loss_func=loss_func)
-        #optimizer = tf.train.AdamOptimizer(0.001)
-        #train_step = optimizer.minimize(loss); train_step_meta = loss
+        #optimizer = MetaOptimizer(shared_scopes, name='MetaOptSimple')
+        #train_step, train_step_meta = optimizer.minimize(loss_func=loss_func)
+        optimizer = tf.train.AdamOptimizer(0.001)
+        train_step = optimizer.minimize(loss); train_step_meta = loss
 
         merged = tf.summary.merge_all()
         writer = tf.summary.FileWriter('./logs/simple', graph=sess.graph)
@@ -164,7 +86,7 @@ def train(parameter_dict):
         for i in range(parameter_dict['num_batches']):
             images, labels = metaDataManager.get_train_batch()
 
-            tr_data = np.reshape(images, (-1, 28*28))
+            tr_data = np.reshape(images, (-1, 28,28,1))
 
             tr_label = np.zeros((len(labels), num_classes))
             tr_label[np.arange(len(labels)), labels] = 1
@@ -183,6 +105,7 @@ def train(parameter_dict):
                 print('}')
 
 
+
                 # Run all the stuff
                 summary, acc, _, _, predicted, loss_ = sess.run(
                     [merged, accuracy, train_step, train_step_meta, y, loss],
@@ -195,6 +118,7 @@ def train(parameter_dict):
                 print('Accuracy: {}, Loss: {}'.format(acc,loss_))
                 print('Predictions: {}'.format(predicted))
 
+
                 # Print stuff out
                 #print('\nIteration: {}, accuracy: {}, loss: {}'.format(i, acc, loss_))
                 #print('Predictions & Answers')
@@ -203,24 +127,22 @@ def train(parameter_dict):
 
             else:
                 acc, _, _, predicted, loss_ = sess.run(
-                            [accuracy, train_step, train_step_meta, y, loss], 
+                            [accuracy, train_step, train_step_meta, y, loss],
                             feed_dict={x: tr_data, y_: tr_label})
-
-
 
 
 
 if __name__ == "__main__":
 
     parameter_dict = {
-        'C': 1,
+        'C': 5,
         'sublayer_type': AdditionSublayerModule,
         'hidden_size': 10,
         'gamma': 0,
         'batch_size': 20,
         'num_batches': 10001,
         'learning_rate': 0.001,
-        'print_every': 1,
+        'print_every': 10,
         'M': 1,
         'L': 2,
         'module_type': PerceptronModule,
